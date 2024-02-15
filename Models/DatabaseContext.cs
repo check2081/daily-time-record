671d9f68884127b77415888d472357454445166f
Exequiel Arroyo <zekielarroyo@gmail.com>
2024-02-15 14:16:23 +0800

    Added DatabaseContext

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TimePulse.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Record> Record { get; set; } = null!;
+    }
+}
